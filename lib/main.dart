09ec18319aa0efe48e248b6b27035040d2c68b85
Luiz A. R. da Silva <luiz.ricardo@groupsoftware.com.br>
2023-05-16 10:23:03 -0300

    Rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
 import 'screens/categories_screen.dart';
 
 void main() {
@@ -11,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        //title: 'Vamos Cozinhar? Aula 182',
+        //title: 'MENU PRINCIPAL',
         theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.pink,
+          primary: Colors.cyan,
           secondary: Colors.amber,
         ),
         canvasColor: const Color.fromRGBO(255, 254, 229, 1),
@@ -26,7 +27,11 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-        home: const CategoriesScreen()
+        home: const CategoriesScreen(),
+        routes: {
+          '/categories-meals': (ctx) => const 
+          CategoriesMealsScreen(),
+        }
       );
   }
 }
---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget{
+  
+  const CategoriesMealsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(category.title)),
+      body: Center(
+        child: Text("Receitas por categoria ${category.id}")
+      )
+    );
+  }
+
+}
